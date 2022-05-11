e83ddc7a422ff4439d896e9629dc3a75b06bb878
Ryan Rambali <ryanrambali@gmail.com>
2022-05-11 12:49:28 -0400

    Added firebase and component + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { JoinComponent } from './components/join/join.component';
+import { WaitComponent } from './components/wait/wait.component';
+import { GameComponent } from './components/game/game.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoinComponent,
+    WaitComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +26,9 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'poker-game-af21a',
+    appId: '1:682539901268:web:0b1a948fd18472d39f68c1',
+    databaseURL: 'https://poker-game-af21a-default-rtdb.firebaseio.com',
+    storageBucket: 'poker-game-af21a.appspot.com',
+    apiKey: 'AIzaSyA72bGcdnkiwrsyCm2UrFxhLJxsgHXXX7A',
+    authDomain: 'poker-game-af21a.firebaseapp.com',
+    messagingSenderId: '682539901268',
+    measurementId: 'G-3Z3EF1EZYL',
+  },
   production: true
 };
